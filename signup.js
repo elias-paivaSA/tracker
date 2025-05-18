9ce2de5614833201158ca0e3c6213e9a60550640
Elias Paiva <eliaspsa13@gmail.com>
2025-05-17 22:37:27 -0300

    Add files via upload

--- signup.js
+++ signup.js
@@ -5,12 +5,16 @@ const passwordTwoInput = document.getElementById('passwordTwo');
 const buttonCreateAccount = document.querySelector('.login-btn');
 const googleBtn = document.querySelector('.google-btn');
 const facebookBtn = document.querySelector('.facebook-btn');
+const nameMessage = document.querySelector('.name-msg');
+const emailMessage = document.querySelector('.email-msg');
+const passwordMessage = document.querySelector('.password-msg');
+const passwordTwoMessage = document.querySelector('.password-two-msg');
 
 buttonCreateAccount.addEventListener('click', (e) => {
     e.preventDefault();
-    if(nameInput.value === "") {console.log("object");}
-    if(emailInput.value === "") {console.log("object");}
-    if(passwordInput.value === "") {console.log("object");}
-    if(passwordTwoInput.value === "") {console.log("object");}
+    if(nameInput.value === "") nameMessage.style.display = 'block'
+    if(emailInput.value === "") emailMessage.style.display = 'block';
+    if(passwordInput.value === "") passwordMessage.style.display = 'block';
+    if(passwordTwoInput.value === "") passwordTwoMessage.style.display = 'block';
 }) 
 
