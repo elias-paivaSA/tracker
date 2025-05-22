43b5777b4f7a4dbcfe596b6fdc1d324dd72ce269
Elias <eliaspsa13@gmail.com>
2025-05-22 12:00:44 -0300

    setting database mongodb

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 app.use(express.json()); // to parse JSON bodies
@@ -7,7 +10,12 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-const PORT = 3000;
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
