4178a974c55934286232a3903d5289cccec7d100
Elias <eliaspsa13@gmail.com>
2025-05-22 13:09:52 -0300

    setting up database mongodb

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ mongoose.connect(process.env.MONGO_URI)
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Serverr running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
